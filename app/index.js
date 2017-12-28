5fa81b6b65d077f7fb5b3403449cdd1fb17d460f
Erik Wotring <ewotring@gmail.com>
2017-12-28 17:43:38 -0600

    Finish setup for app

--- app/index.js
+++ app/index.js
@@ -1,5 +1,14 @@
-require('./css/main.css')
+require('bootstrap/dist/css/bootstrap.min.css');
+require('./css/main.css');
+import React from 'react';
+import ReactDOM from 'react-dom';
+export default class HelloWorld extends React.Component {
+  render() {
+    return(<input type="button" className="btn btn-primary" value="Hello World" />);
+  }
+}
 
-var element = document.createElement('h1');
-element.innerHTML = 'Hello world with a tuba';
-document.body.appendChild(element);
+ReactDOM.render(
+  <HelloWorld />,
+  document.getElementById('app')
+);
